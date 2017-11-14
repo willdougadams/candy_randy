0fbd92e38f2191f9af361b1e3a4cf3e851e8a24d
Will Adams <wadams@csumb.edu>
2017-11-13 20:27:04 -0800

    Clicking kinda works

--- src/game.py
+++ src/game.py
@@ -30,6 +30,9 @@ class Game(State):
     for n in range(5):
       self.npcs.append(NPC((100 + n*100, 100 + n*20), 10, self.buffer_frame, "res/npcs/beholder.npc"))
 
+    self.window_scale_factor = 2
+    self.window_offset = (0, 0)
+
   def update(self, user_input, mouse_position, elapsed):
     for event in user_input:
       if event.type == pygame.KEYDOWN:
@@ -44,10 +47,13 @@ class Game(State):
         elif event.key == pygame.K_3:
           self.pcs[self.active_pc].active_skill = 2
       elif event.type == pygame.MOUSEBUTTONDOWN:
+        new_x = (mouse_position[0] / self.window_scale_factor + self.window_offset[0])
+        new_y = (mouse_position[1] / self.window_scale_factor + self.window_offset[1])
+        window_mouse_pos = (new_x, new_y)
         if event.button == 1:
-          self.pcs[self.active_pc].target_dest = mouse_position
+          self.pcs[self.active_pc].target_dest = window_mouse_pos
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
-          self.active_skills.append(self.pcs[self.active_pc].fire(mouse_position))
+          self.active_skills.append(self.pcs[self.active_pc].fire(window_mouse_pos))
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 4:
           skills_amt = len(self.pcs[self.active_pc].skills)
           self.pcs[self.active_pc].active_skill = (self.pcs[self.active_pc].active_skill + 1) % skills_amt
@@ -55,7 +61,7 @@ class Game(State):
           skills_amt = len(self.pcs[self.active_pc].skills)
           self.pcs[self.active_pc].active_skill = (self.pcs[self.active_pc].active_skill - 1) % skills_amt
 
-    new_level_offset = (0, 0)
+    new_level_offset = self.window_offset
     self.level.update(new_level_offset)
 
     self.active_skills = [a for a in self.active_skills if a is not None]
@@ -84,8 +90,12 @@ class Game(State):
     for p in self.pcs:
       p.draw()
 
+    scale_factor = self.window_scale_factor
     w, h = self.screen.get_size()
-    self.screen.blit(self.buffer_frame, (self.level.offset[0], self.level.offset[1]), (0, 0, w, h))
+    frame = pygame.Surface((w, h))
+    frame.blit(self.buffer_frame, (0, 0), (self.level.offset[0], self.level.offset[1], w, h))
+    frame = pygame.transform.scale(frame, (w*scale_factor, h*scale_factor))
+    self.screen.blit(frame, (0, 0))
 
     self.hud.draw()
 
